e6361e78d95bcfac58c6bafd340a52755e6f09a6
zubair-pc\zubair <szubair@gmail.com>
2021-06-03 19:02:17 +0500

    Basic logger is implimimented

--- src/main/kotlin/com/example/SpringbootLogging/Controllers/HomeResource.kt
+++ src/main/kotlin/com/example/SpringbootLogging/Controllers/HomeResource.kt
@@ -1,13 +1,20 @@
 package com.example.SpringbootLogging.Controllers
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HomeResource {
+    /*
+    By default configuration logback is present in our project
+    We will make a logger object by using static method get logger of LoggerFactory and pass it name of current class
+     */
+    val logger = LoggerFactory.getLogger(this.javaClass.name);
 
     @RequestMapping("/")
     fun home():String{
+        logger.error("Error happend");
         return "Hello from Spring boot";
     }
 }
